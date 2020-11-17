761637d56fc6cc382d36119658dab6de265ce8b6
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2020-11-17 10:34:07 +0000

    vuepress: add sidebar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,3 +1,9 @@
+const sidebar = ['', 'name', 'layout']
+
+for (const locale of Object.keys(locales)) {
+  locales[locale].sidebar = { [locale]: sidebar }
+}
+
 module.exports = {
   base: "/java-coding-conventions/",
   title: 'Bellerbys Java Coding Conventions ',
