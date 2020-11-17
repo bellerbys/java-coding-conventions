7b5f78919172792eaf9d2a30b87a081d8c693019
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2020-11-17 10:11:57 +0000

    deploy: add vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'Bellerbys Java Coding Conventions ',
+  description: 'Java Programming Guide ',
+  themeConfig: {
+    repo: 'bellerbys/java-coding-conventions',
+    editLinks: true,
+    lastUpdated: true,
+  },
+}
